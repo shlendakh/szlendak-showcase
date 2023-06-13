96510fb907b73c74c7c75b46f484a8adccbfa802
Paweł Szlendak <kontakt@pawelszlendak.pl>
2023-06-13 21:05:42 +0200

    working around mobile height problem

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss'
 
+// Aktualizacja wysokości okna dla urządzeń mobilnych
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

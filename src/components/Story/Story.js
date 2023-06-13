408e6025ddf0b93f517acb350951a3f3073cdae9
Paweł Szlendak <kontakt@pawelszlendak.pl>
2023-06-14 01:03:55 +0200

    v1.1.0

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import DOMPurify from 'dompurify';
 import './Story.scss';
 
 function Story({ activeStory, nextStory, previousStory}) {
@@ -34,16 +35,106 @@ function Story({ activeStory, nextStory, previousStory}) {
     }
   };
 
+  const createMarkup = (html) => {
+    return  {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
+
   return (
     <div 
-      className={`story story-${activeStory}`} 
+      className={`story story-${activeStory}`}
+      id={`story-${activeStory}`} 
       onClick={handleClick}
       onTouchStart={handleTouchStart}
       onTouchMove={handleTouchMove}
+      dangerouslySetInnerHTML={createMarkup(stories[activeStory].content)}
     >
-      <p>Story {activeStory}</p>
     </div>
   );
 }
+
+const stories = [
+  {
+    id: 1,
+    title: "Test",
+    content: `
+      <h2>poznajmy się</h2>
+      <h1>Szlendak x Tukan</h1>
+      <div class="content">
+        <h3>kilka słów o mnie</h3>
+        <p>kliknij lub przesuń (jak na Insta) <i class="fa-solid fa-arrow-right"></i></p>
+      </div>
+    `
+  },
+  {
+    id: 2,
+    title: "Test",
+    content: `
+      <h2>poznajmy się</h2>
+      <h1>Szlendak x Tukan</h1>
+      <div class="content">
+        <h3>kilka słów o mnie</h3>
+        <p>kliknij lub przesuń (jak na Insta) <i class="fa-solid fa-arrow-right"></i></p>
+      </div>
+    `
+  },
+  {
+    id: 3,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 4,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 5,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 6,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 7,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 8,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 9,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+  {
+    id: 10,
+    title: "Test",
+    content: `
+      <p>Szlendak x Tukan</p>
+    `
+  },
+];
 
 export default Story;
